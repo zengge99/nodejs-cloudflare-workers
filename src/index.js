c90ef2d29652a2374853bb863e13f0901892ba29
zengge99 <83830807+zengge99@users.noreply.github.com>
2025-04-25 17:03:04 +0800

    ok

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ export default {
   async fetch(request, env) {
     const browser = await puppeteer.launch(env.MYBROWSER);
     const page = await browser.newPage();
-    await page.goto("https://example.com");
+    await page.goto("https://www.baidu.com");
     const metrics = await page.metrics();
     await browser.close();
     return Response.json(metrics);
