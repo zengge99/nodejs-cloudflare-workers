d9eb99a18a9964ea6275241e69b6b10fa9da5a48
zengge99 <83830807+zengge99@users.noreply.github.com>
2025-04-27 09:32:16 +0800

    ok

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ export default {
 
       await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36');
       
-      await page.goto("https://www.bing.com/search?q=%E5%85%AD%E5%A7%8A%E5%A6%B9+site%3amovie.douban.com%2fsubject", {
+      await page.goto("https://www2.bing.com/search?q=%E5%85%AD%E5%A7%8A%E5%A6%B9+site%3amovie.douban.com%2fsubject", {
         waitUntil: 'networkidle2', 
         timeout: 10000 
       });
