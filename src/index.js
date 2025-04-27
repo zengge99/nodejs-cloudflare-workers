20b5973360e7859a1f64453631ec775ac1a5f347
zengge99 <83830807+zengge99@users.noreply.github.com>
2025-04-27 10:11:24 +0800

    ok

--- src/index.js
+++ src/index.js
@@ -41,7 +41,7 @@ export default {
       await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36');
       await page.setViewport({ width: 1366, height: 768 });
 
-      const initialUrl = "https://www.bing.com/search?q=%E5%85%AD%E5%A7%8A%E5%A6%B9+site%3amovie.douban.com%2fsubject";
+      const initialUrl = "https://www2.bing.com/search?q=%E5%85%AD%E5%A7%8A%E5%A6%B9+site%3amovie.douban.com%2fsubject";
       await page.goto(initialUrl, {
         waitUntil: 'networkidle2',
         timeout: 15000
